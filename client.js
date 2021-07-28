bd0fdde38f127eeeb179ada833b8d539e5b7123b
vagrant <t.oleary@me.com>
2021-07-28 20:02:03 +0000

    moved the connect function

---
+++ client.js
@@ -0,0 +1,16 @@
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: '50541',
+  });
+
+  conn.on("data", (message) => {
+    console.log(`you said ${message}`);
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const net = require("net");
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: '50541',
-  });
-
-  conn.on("data", (message) => {
-    console.log(`you said ${message}`);
-  });
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting ... ");
 connect();
