8ad69c67d650da5ed0d704304d5f8ea237d655dd
vagrant <t.oleary@me.com>
2021-07-28 22:28:27 +0000

    added setup input and handle user input

--- play.js
+++ play.js
@@ -3,6 +3,21 @@
 const connect = require('./client');
 
 console.log("Connecting ... ");
-connect();
 
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function () {
+  process.stdin.on('data', (key) => {
+    if (key === './u003') {
+      process.exit();
+    } else {
+    process.stdout.write(key);
+  }
+})};
